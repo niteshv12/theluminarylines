3d8aef40867f554f055a098f76b07b3ef1b63664
ShubhamRaj08 <145212041+ShubhamRaj08@users.noreply.github.com>
2024-04-05 13:54:34 +0530

    animation

--- footer.php
+++ footer.php
@@ -86,7 +86,7 @@
             </li>
             <li>
               <img src="assets/icons/Frame (6).svg" alt="" />
-              <p style="color: #fff;"> +91 9871845696</p>
+              <p style="color: #fff;">+91 7428599745 | +91 7428599751 </p>
             </li>
             <!-- <li>
                 <img src="assets/icons/Frame (7).svg" alt="" />
